acebccacb7bcbead02822297b6a7b613ca8c3603
AdityaRai24 <adityarai407@gmail.com>
2024-03-21 21:19:33 +0530

    Fixed urls

--- src/app/mcq/page.jsx
+++ src/app/mcq/page.jsx
@@ -10,7 +10,7 @@ const McqPage = ({ searchParams }) => {
       <div className="flex items-center justify-center max-w-[85%] mx-auto h-[80vh] ">
         <div className="w-[55%]">
           <h1 className="text-6xl font-bold tracking-wide leading-[70px]">
-            Choose a Category you would like to play Quiz on.
+            Choose a Category you would like to play Quiz on..
           </h1>
           <p className="text-muted-foreground text-md">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
